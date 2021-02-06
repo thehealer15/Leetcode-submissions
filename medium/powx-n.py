edfa40e7418c3e4fbd717ac20d0d6411014a3909
Akshay Pawar <60501569+thehealer15@users.noreply.github.com>
2021-02-06 20:43:07 +0530

    Create powx-n.py

---
+++ medium/powx-n.py
@@ -0,0 +1,7 @@
+class Solution:
+    def myPow(self, x: float, n: int) -> float:
+        return x**n
+    
+#     Runtime: 44 ms, faster than 13.46% of Python3 online submissions for Pow(x, n).
+# Memory Usage: 14.2 MB, less than 83.25% of Python3 online submissions for Pow(x, n).
+#     https://leetcode.com/problems/powx-n
